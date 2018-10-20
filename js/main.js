132b7a893df702582d74ddc7f782d0b3383eb8e2
yan yan <ece.yan@gmail.com>
2018-10-20 17:36:27 +1000

    1change

--- js/main.js
+++ js/main.js
@@ -1,27 +1,55 @@
 
-document.getElementById('checkbox').addEventListener('change',agreeTerm);
+document.getElementById('checkbox').addEventListener('click',agreeTerm);
 function agreeTerm(){
-    if(document.getElementById('checkbox').checked==true){
-        document.getElementById('item15').disabled=false;
-        
+    if(document.getElementById('checkbox').clicked=true){
+        document.getElementById('item15').disabled=false; 
     }
-    else{
-        document.getElementById('item15').disabled=true;
 
+    else if(Number(localStorage.clickcount)%2==0){
+        document.getElementById('item15').disabled=true;
     }
+   
 }
 
 document.getElementById('item15').addEventListener('click',submit);
 function submit(){
-    if(localStorage.clickcount = 1 && document.getElementById('item15').disabled==false){
-        document.getElementById('itme15').value='Processing...';
+    if(getElementById('item15').clickcount =='1'){
+        document.getElementById('itme15').value=='Processing...';
     }
 }
 
-document.getElementById('item16').addEventListener('hover',help);
-function help(){
-    if(document.getElementById('item16').click==true){
-        document.getElementById('help').style.display='block';
 
+
+document.getElementById('confirmpwd').addEventListener('input',psw);
+function psw(){
+    if (document.getElementById('pwd').value==document.getElementById('confirmpwd').value){
+        document.getElementById('popup').style.display='none';
+    }
+    else{
+        document.getElementById('popup').style.display='block';
+
+    }
+   
+}
+
+document.getElementById('close').addEventListener('click',close);
+function close(){
+    if(document.getElementById('close').click=true){
+        document.getElementById('popup').style.display='none';
+
+    }
+}
+
+document.getElementById('item16').addEventListener('mouseover',help);
+function help(){
+    if(document.getElementById('item16').mouseover=true){
+        document.getElementById('video').style.display='block';
+    }
+}
+
+document.getElementById('item16').addEventListener('mouseleave',leave);
+function leave(){
+    if(document.getElementById('item16').mouseleave=true){
+        document.getElementById('video').style.display='none';
     }
 }
